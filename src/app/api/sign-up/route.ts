1fdbb4289b9bef914da1da2f3f041e3381fa0954
AnkushS27 <ankush27singh11@gmail.com>
2024-12-03 00:09:12 +0530

    fix: post handler error removed

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 import dbConnect from '@/lib/dbConnect';
 import UserModel from '@/model/User';
 
-export default async function POST(request: Request) {
+export async function POST(request: Request) {
   await dbConnect();
   try {
     const { username, email, password, role } = await request.json();
